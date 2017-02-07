89c0fc9e58c25eca61990ca13dfe545822ef7571
Vaibhav Viswanathan <vviswanathan@hmc.edu>
2017-02-06 17:53:57 -0800

    finished linearization code

--- Beluga Model/lin_test.m
+++ Beluga Model/lin_test.m
@@ -1,24 +1,37 @@
 % linearize the Beluga dynamic model
 
-% define constants
-m = 20.2; %kg
-Ix = 
+% TODO define constants
+m = 20.2; %kg 
 
-%define mass matrix
-syms m Ix Iy Iz xg zg X_udot Y_vdot Y_rdot Z_wdot Z_qdot K_pdot M_wdot M_qdot N_vdot N_rdot
-M = [   m-X_udot    0           0               0           m*zg            0; 
-        0           m-Y_vdot    0               -m*zg       0               m*xg-Y_rdot; 
-        0           0           m-Z_wdot        0           -m*xg-Z_qdot    0; 
-        0           -m*zg       0               Ix-K_pdot   0               0; 
-        m*zg        0           -m*xg-M_wdot    0           Iy-M_qdot       0; 
-        0           m*xg-N_vdot 0               0           0               Iz-N_rdot];
+% create constant variables
+syms m Ix Iy Iz xg zg W ...
+    X_udot Y_vdot Y_rdot Z_wdot Z_qdot K_pdot M_wdot M_qdot N_vdot N_rdot ...
+    X_u_abs_u K_p_abs_p M_w_abs_w M_q_abs_q N_v_abs_v N_r_abs_r ...
+    K rho d L
 
+% create state variables
+syms x1 x2 x3 x4 x5 x6 x7 x8 x9 x10 x11 x12 n1 n2 n3 n4
 
-% define force vector
-syms f1 f2 f3 f4 f5 f6
-F = [f1 f2 f3 f4 f5 f6];
-
-% determine acceleration vector
-a = simplify(inv(M))*transpose(F);
-
-
+% state vectors
+f_state = [ X_u_abs_u/(m-X_udot)*(x1*abs(x1));
+            0;
+            0;
+            1/(Ix-K_pdot)*(-zg*W*cos(x8)*sin(x7)+K_p_abs_p*x4*abs(x4));
+            1/(Iy-M_qdot)*(-zg*W*sin(x8)+M_w_abs_w*x3*abs(x3)+M_q_abs_q*x5*abs(x5));
+            1/(Iz-N_rdot)*(N_v_abs_v*x5*abs(x5)+N_r_abs_r*x6*abs(x6))];
+f_control = [   K*rho*d^4/(m-X_udot)*(n1^2-x1*n1/d+n2^2-x1*n2/d+n3^2-x1*n3/d+n4^2-x1*n4/d);
+                0;
+                0;
+                0;
+                L*K*rho*d^4/(Iy-M_qdot)*(n1^2-x1*n1/d+n3^2-x1*n3/d);
+                L*K*rho*d^4/(Iz-N_rdot)*(n2^2-x1*n2/d+n4^2-x1*n4/d);
+                0;
+                0;
+                0;
+                0;
+                0;
+                0];
+                       
+% Now compute jacobian
+dfdx = jacobian(f_state, [x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12]);
+dfdu = jacobian(f_control, [n1; n2; n3; n4]);
