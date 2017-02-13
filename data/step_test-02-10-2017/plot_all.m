c82a09c5ae75ee7fe0ee2c88a348458ec9ca9de2
klund1 <klund@hmc.edu>
2017-02-12 16:03:40 -0800

    processed step test data

---
+++ data/step_test-02-10-2017/plot_all.m
@@ -0,0 +1,15 @@
+files = dir();
+filenames = {files.name};
+pat = '(forward.*)\.bag';
+
+matches = regexp(filenames,pat,'tokens');
+
+for match = matches
+    token = match{1};
+    if ~isempty(token)
+        matlab_cells_are_dumb = token{1};
+        prefix = matlab_cells_are_dumb{1};
+        plot_ekf_data([prefix '_ekf.dat'], [prefix '_input.dat'])
+    end
+end
+        
---
+++ data/step_test-02-10-2017/plot_ekf_data.m
@@ -0,0 +1,84 @@
+function plot_ekf_data( data_filename, input_filename)
+
+split_filename = regexp(input_filename,'\.','split');
+file_prefix = split_filename(1);
+
+raw = importdata(data_filename);
+t_str = raw.textdata(2:end,1);
+t0 = str2num(t_str{1});
+t = (cellfun(@str2num,t_str)-t0)*1e-9;
+p = raw.data(:,1:3);
+v = raw.data(:,44:46);
+vx = v(:,1);
+vy = v(:,2);
+vz = v(:,3);
+
+s = sqrt(vx.^2+vy.^2+vz.^2);
+s_2d = sqrt(vx.^2+vy.^2);
+
+in_raw = importdata(input_filename);
+if ~all(size(in_raw.data) == [2,2])
+    fprintf('Bad input data: %s\n',input_filename)
+    return
+end
+input_speed = -in_raw.data(1,2);
+t_in_0 = (in_raw.data(1,1)-t0)*1e-9;
+t_in_f = (in_raw.data(2,1)-t0)*1e-9;
+
+ind = t>=t_in_0 & t<=t_in_f;
+
+t_step = t(ind);
+t0 = t_step(1);
+t_step = t_step - t0;
+
+s_2d_step = s_2d(ind);
+s0 = s_2d_step(1);
+s_2d_step = s_2d_step - s0;
+
+figure
+plot(t_step,s_2d_step)
+grid on
+xlabel('Time (s)')
+ylabel('Speed (m/s)')
+title(sprintf('Step Response -- Input: %g',input_speed))
+print(file_prefix{1},'-dpng')
+
+%{
+figure
+plot(t,s,'b')
+hold on
+plot(t(ind),s(ind),'r')
+grid on
+
+
+figure
+plot(t,-vy,'b')
+hold on
+plot(t(ind),-vy(ind),'r')
+grid on
+xlabel('Time (s)')
+ylabel('Forward Velocity (m/s)')
+
+
+figure
+subplot(3,1,1)
+plot(t,vx,'b')
+hold on
+plot(t(ind),vx(ind),'r')
+grid on
+
+subplot(3,1,2)
+plot(t,vy,'b')
+hold on
+plot(t(ind),vy(ind),'r')
+grid on
+
+subplot(3,1,3)
+plot(t,vz,'b')
+hold on
+plot(t(ind),vz(ind),'r')
+grid on
+
+%}
+end
+
